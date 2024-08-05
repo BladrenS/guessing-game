b3355198b66a61d43883295afacc6fe105d380b7
[BladrenS] <denis_ka_98@mail.ru>
2024-08-05 21:43:58 +0600

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+      this.root = null;
+      this.left = null;
+      this.right = null
     }
 
-    guess() {
+    setRange(min, max) {
+      this.right = max;
+      this.left = min;
+    }
 
+    guess() { 
+      let guessNumber = Math.round((this.left + this.right) / 2);
+      this.root = guessNumber;
+      return guessNumber
     }
 
     lower() {
-
+      this.right = this.root;
+      this.root = null;
     }
 
     greater() {
-
+      this.left = this.root;
+      this.root = null;
     }
 }
 
